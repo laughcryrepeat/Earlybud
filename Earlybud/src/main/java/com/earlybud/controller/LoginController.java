c4f58fc56196925b52c24c3a4bf5f5532a183339
monmokk <45473287+monmokk@users.noreply.github.com>
2019-01-30 15:51:59 +0900

    member

--- Earlybud/src/main/java/com/earlybud/controller/LoginController.java
+++ Earlybud/src/main/java/com/earlybud/controller/LoginController.java
@@ -1,15 +1,12 @@
 package com.earlybud.controller;
 
-import java.util.Locale;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
 	@RequestMapping("login")
-	public String login(Locale locale, Model model) {
+	public String login() {
 		return "login/login";
 	}
 }
